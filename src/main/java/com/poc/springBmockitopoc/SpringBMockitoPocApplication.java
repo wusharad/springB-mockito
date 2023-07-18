a5b9a509ae5c0e4f69cdc25d6a390d7ad5d81ee5
climb_anywhere <anywhereclimb@gmail.com>
2023-07-18 12:15:18 +0530

    Initial commit

---
+++ src/main/java/com/poc/springBmockitopoc/SpringBMockitoPocApplication.java
@@ -0,0 +1,13 @@
+package com.poc.springBmockitopoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBMockitoPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBMockitoPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/poc/springBmockitopoc/SpringBMockitoPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poc.springBmockitopoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBMockitoPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
